e4ac5730d96accc79c44f16b10196884c5151bc7
Banger456 <aayushk532@gmail.com>
2023-02-16 20:36:36 -0500

    Added HashBrowser.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import store from "./store";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
-import { BrowserRouter } from "react-router-dom";
+
+//import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
