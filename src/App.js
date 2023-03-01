22b706a8b48b9ddc9b0b1f7567712ac682cda016
Banger456 <aayushk532@gmail.com>
2023-03-01 15:18:13 -0500

    Added logo image in navbar

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <Link to={"/"} className="navbar-brand">
-          Tensor Travels
+          <img src="logo-image" class="img-fluid"></img>
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
