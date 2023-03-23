abe2b3263e9eb8ab20a5006cf88788acd4a1e934
Banger456 <aayushk532@gmail.com>
2023-03-23 16:20:42 -0400

    fixed import

--- src/components/ContestView.js
+++ src/components/ContestView.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPhotos } from "../actions/photo";
+import { vote } from "../actions/Photo";
 
 const groupPhotosByCategory = (photos) => {
     const groupedPhotos = {};
